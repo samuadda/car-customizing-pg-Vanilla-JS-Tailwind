dd78209276a6b98d7b933cfa541dbb986e3889a4
saddiq musa <saddiq0musa@gmail.com>
2024-12-20 18:05:09 +0300

    refactor wheel buttons and enhance color selection functionality

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // top Bar
 const topBar = document.getElementById('topBar')
 
-const handleTopBarScroll = () => { 
+const handleTopBarScroll = () => {
     const atTop = window.scrollY === 0
 
     topBar.classList.toggle('visible-bar', atTop);
@@ -28,6 +28,18 @@ const handleColorSelection = (event) => {
         const buttons = event.currentTarget.querySelectorAll('button');
         buttons.forEach((btn) => btn.classList.remove('btn-selected'));
         button.classList.add('btn-selected');
+
+        // change exterior image
+        if (event.currentTarget === exteriorColorOptions) {
+            const color = button.querySelector('img').alt;
+            exteriorImage.src = exteriorImages[color];
+        }
+
+        // change interior image
+        if (event.currentTarget === interiorColorOptions) {
+            const color = button.querySelector('img').alt;
+            interiorImage.src = interiorImages[color];
+        }
     }
 }
 
@@ -36,15 +48,31 @@ interiorColorOptions.addEventListener('click', handleColorSelection)
 
 //image mapping
 const exteriorImages = {
-    "stealth-grey": "./assets/model-y-stealth-grey.jpg",
-    "pearl-white": "./assets/model-y-pearl-white.jpg",
-    "deep-blue-metallic": "./assets/deep-blue-metallic.jpg",
-    "solid-black": "./assets/model-y-solid-black.jpg",
-    "ultra-red": "./assets/model-y-ultra-red.jpg",
-    "quicksilver": "./assets/model-y-quicksilver.jpg",
+    "stealth-grey": "./src/assets/model-y-stealth-grey.jpg",
+    "pearl-white": "./src/assets/model-y-pearl-white.jpg",
+    "deep-blue-metallic": "./src/assets/model-y-deep-blue-metallic.jpg",
+    "solid-black": "./src/assets/model-y-solid-black.jpg",
+    "ultra-red": "./src/assets/model-y-ultra-red.jpg",
+    quicksilver: "./src/assets/model-y-quicksilver.jpg",
 };
 
 const interiorImages = {
-    'dark': './assets/model-y-interior-dark.jpg',
-    'light': './assets/model-y-interior-light.jpg',
+    dark: "./src/assets/model-y-interior-dark.jpg",
+    light: "./src/assets/model-y-interior-light.jpg",
+};
+
+// wheels options
+const wheelsButtons = document.getElementById("wheels-buttons");
+
+const handleWheelsSelection = (event) => { 
+    if (event.target.tagName === 'BUTTON') {
+        const buttons = event.currentTarget.querySelectorAll('button');
+        buttons.forEach((btn) => btn.classList.remove('bg-gray-700', 'text-white'));
+        event.target.classList.add("bg-gray-700", "text-white");
+
+        const selectedWheel = event.target.querySelector("img").id === "performance";
+        exteriorImage.src = selectedWheel ? "./src/assets/model-y-stealth-grey-performance.jpg" : "./src/assets/model-y-stealth-grey.jpg";
+    }
 }
+
+wheelsButtons.addEventListener('click', handleWheelsSelection)
